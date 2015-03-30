9bc4ca6c06f56afc40c00e7f370cb4eeefb57622
Filip Jonassen <filip@adminisatorsmbp.lan>
2015-03-30 11:08:19 +0200

    added code needed for lecturers in .js script

--- schoolregister/theme/static/theme/js/schoolregister.js
+++ schoolregister/theme/static/theme/js/schoolregister.js
@@ -4,11 +4,22 @@ $(document).ready(function(){
 	$("#increase_passed_exams_button").click(function(event) {
 		event.preventDefault();
 		$.ajax({
-			url: $('#passed_exams_url').val()   // needs change if copied
+			url: $('#passed_exams_url').val()   
 		})
 		.done(function(data){
-			var passed_exams_updated = data['passed_exams_updated']; // needs change if copied
-			$("#passed_exams_cell").html(passed_exams_updated) // needs change if copied
+			var passed_exams_updated = data['passed_exams_updated']; 
+			$("#passed_exams_cell").html(passed_exams_updated) 
+		});
+	});
+
+	$("#increase_number_of_courses").click(function(event) {
+		event.preventDefault();
+		$.ajax({
+			url: $('#number_of_courses_url').val()  
+		})
+		.done(function(data){
+			var number_of_courses_updated = data['number_of_courses_updated']; 
+			$("#number_of_courses_cell").html(number_of_courses_updated) 
 		});
 	});
 
@@ -16,7 +27,7 @@ $(document).ready(function(){
 
 
 
-	
+
 
 
 });
