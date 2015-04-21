332789924c23f1cacaf6866f1bc72b7375e57139
Filip Jonassen <filip@Administrators-MacBook-Pro.local>
2015-04-21 09:12:05 +0200

    Fixed faulty code, resulting in having to refresh site each push of button.

--- schoolregister/theme/static/theme/js/schoolregister.js
+++ schoolregister/theme/static/theme/js/schoolregister.js
@@ -4,16 +4,17 @@ $(document).ready(function(){
 	$(".add-points-link").click(function(event) {
 		event.preventDefault();
 		var note_id = $(this).data("noteid");
-		var points_element_id = "#id-points-for-note" + note_id;
+		var points_element_id = "#id-points-for-note-" + note_id;
 		$.ajax({
 			method: "POST",
-			url: $('#add_points_url').val() + "/" + note_id  
+			url: $('#add_points_url').val() + "/" + note_id
 		})
 		.done(function(data){
-			var points_updated = data['points_updated'];
-			$("#points_element_id").html(points_updated);
+		var points_updated = data['points_updated'];
+		$(points_element_id).html(points_updated);
 		});
 	});
+
 
 	$("#increase_passed_exams_button").click(function(event) {
 		event.preventDefault();
